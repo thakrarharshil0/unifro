5899428c5e1c48c7d33faf61cb827a9f844b90a1
Harshil <harshil@unified.com>
2026-08-12 10:16:23 +0530

    Add Google Tag Manager

--- app/layout.jsx
+++ app/layout.jsx
@@ -121,7 +121,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KGTWXGS2');`
+          }}
+        />
+      </head>
       <body className="antialiased bg-white text-slate-900" suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KGTWXGS2"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Suspense fallback={null}>
           <AnalyticsTracker />
         </Suspense>
